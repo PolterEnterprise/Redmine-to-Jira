db54b9c218763e63dedcbb2cf0a260b41969591e
Harrison Wilford <25427388+polterx@users.noreply.github.com>
2023-07-03 13:47:18 +0000

    Update _exporter.py
    Signed-off-by: Harrison Wilford <25427388+polterx@users.noreply.github.com>

--- utils/_exporter.py
+++ utils/_exporter.py
@@ -1,3 +1,9 @@
+# ############################################################################## #
+# Created by Polterx, on Saturday, 1st of July, 2023                             #
+# Website https://poltersanctuary.com                                            #
+# Github  https://github.com/PolterEnterprise                                    #
+# ############################################################################## #
+
 import os
 import sys
 import requests
