5f28d5585d05415c8ecf757846f5358230d3b514
Harrison Wilford <25427388+polterx@users.noreply.github.com>
2023-07-04 07:44:08 +0000

    Delete _importer.py
    Signed-off-by: Harrison Wilford <25427388+polterx@users.noreply.github.com>

--- utils/_importer.py
+++
@@ -1,5 +0,0 @@
-# ############################################################################## #
-# Created by Polterx, on Saturday, 1st of July, 2023                             #
-# Website https://poltersanctuary.com                                            #
-# Github  https://github.com/PolterEnterprise                                    #
-# ############################################################################## #
