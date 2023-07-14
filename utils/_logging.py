080a3ef0d896fb500f437d0e2a5aea743556e4d7
Harrison Wilford <25427388+polterx@users.noreply.github.com>
2023-07-14 05:14:01 +0200

    _logging.py - v1.0.1
    Signed-off-by: Harrison Wilford <25427388+polterx@users.noreply.github.com>

--- utils/_logging.py
+++ utils/_logging.py
@@ -4,9 +4,9 @@
 # Github  https://github.com/PolterEnterprise                                    #
 # ############################################################################## #
 
+import coloredlogs
 import logging
 from logging.handlers import RotatingFileHandler
-import coloredlogs
 
 def configure_logging(debug_mode=False):
     level = logging.DEBUG if debug_mode else logging.INFO
@@ -18,25 +18,20 @@ def configure_logging(debug_mode=False):
     if not debug_mode:
         coloredlogs.install(level=level, fmt=log_format, datefmt=datefmt)
     else:
-        # Disable coloredlogs if debug mode is enabled
         coloredlogs.install(level=logging.INFO, fmt=log_format, datefmt=datefmt)
 
 def setup_logger(log_file):
-    # Create a logger instance
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
 
-    # Define log formatting
     log_format = "[%(asctime)s] %(levelname)s %(message)s"
     log_date_format = "%Y-%m-%d %H:%M:%S"
     formatter = logging.Formatter(log_format, log_date_format)
 
-    # Create a rotating file handler
     file_handler = RotatingFileHandler(log_file, maxBytes=1000000, backupCount=3)
     file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(formatter)
 
-    # Add the file handler to the logger
     logger.addHandler(file_handler)
 
     return logger
