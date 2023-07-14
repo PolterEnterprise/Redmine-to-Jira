1c082b7fba3bfb6bbd0a8430643f0f5e74d86a79
Harrison Wilford <25427388+polterx@users.noreply.github.com>
2023-07-14 05:10:39 +0200

    _exporter.py - v1.0.1
    Signed-off-by: Harrison Wilford <25427388+polterx@users.noreply.github.com>

--- utils/_exporter.py
+++ utils/_exporter.py
@@ -7,7 +7,6 @@
 import os
 import sys
 import requests
-import urllib.request
 import json
 
 from configparser import ConfigParser
@@ -42,6 +41,7 @@ class RedmineExporter:
         self.attachments_dir = config.get('Exporter', 'attachments_dir')
         self.log_file = config.get('Exporter', 'log_file')
         self.rate_limit_delay = config.getint('Exporter', 'rate_limit_delay')
+        self.maximum_issues = config.getint('Exporter', 'maximum_issues')
 
         self.logger = setup_logger(self.log_file)
 
@@ -56,12 +56,17 @@ class RedmineExporter:
             params = {"key": self.redmine_api_key, "subproject_id": "!*"}
 
         status_map = {
-            1: '1',  # New issues
-            2: '2',  # In Progress issues
-            3: '3',  # Resolved issues
-            4: '4',  # Feedback issues
-            5: '5',  # Closed issues
-            6: '6'   # Rejected issues
+            1: '1',  # New issues (The issue is newly created and not yet assigned or in progress.)
+            2: '2',  # In Progress issues (The issue is actively being worked on.)
+            3: '3',  # Resolved issues (The issue has been resolved or completed.)
+            4: '4',  # Feedback issues (The issue is awaiting feedback or further input.)
+            5: '5',  # Closed issues (The issue has been closed and is considered complete.)
+            6: '6',  # Rejected issues (The issue has been rejected or deemed invalid.)
+            7: '7',  # Approved issues (The issue has been approved and can proceed.)
+            8: '8',  # Won't Fix issues (The issue will not be fixed or addressed.)
+            9: '9',  # Re-opened issues (The issue was previously closed but has been reopened for further work.)
+            10: '10',  # In View issues (The issue is being reviewed or examined.)
+            11: '11'  # Ready for Testing issues (The issue is ready for testing or quality assurance.)
         }
 
         priority_map = {
@@ -72,13 +77,24 @@ class RedmineExporter:
             5: '5'   # Immediate priority
         }
 
-        if status:
-            params["status_id"] = status_map.get(status, str(status))
+        if status is not None:
+            if status == 'approved':
+                params["status_id"] = status_map[7]
+            elif status == "won't fix":
+                params["status_id"] = status_map[8]
+            elif status == "re-opened":
+                params["status_id"] = status_map[9]
+            elif status == "in view":
+                params["status_id"] = status_map[10]
+            elif status == "ready for testing":
+                params["status_id"] = status_map[11]
+            else:
+                params["status_id"] = status_map.get(status, str(status))
 
         if priority:
             params["priority_id"] = priority_map.get(priority, str(priority))
 
-        params["limit"] = 1000
+        params["limit"] = self.maximum_issues
 
         issues = []
         offset = 0
@@ -139,7 +155,14 @@ class RedmineExporter:
                 attachment_filename = attachment["filename"]
                 attachment_path = os.path.join(self.attachments_dir, str(issue_id))
                 os.makedirs(attachment_path, exist_ok=True)
-                urllib.request.urlretrieve(attachment_url, os.path.join(attachment_path, attachment_filename))
+
+                # Download attachment using requests library
+                attachment_response = requests.get(attachment_url, stream=True)
+                attachment_response.raise_for_status()
+                with open(os.path.join(attachment_path, attachment_filename), "wb") as file:
+                    chunk_size = 50 * 1024 * 1024  # 50MB chunk size
+                    for chunk in attachment_response.iter_content(chunk_size=chunk_size):
+                        file.write(chunk)
         except Exception as e:
             print(f"Error fetching attachments for issue {issue_id}. Error: {str(e)}")
 
@@ -175,9 +198,15 @@ class RedmineExporter:
     def setup_attachments_directory(self):
         os.makedirs(self.attachments_dir, exist_ok=True)
 
+    def setup_projects_directory(self):
+        projects_dir = "projects"
+        os.makedirs(projects_dir, exist_ok=True)
+
     def run(self, args):
         configure_logging(args.debug)
+
         self.setup_attachments_directory()
+        self.setup_projects_directory()
 
         if not args.project:
             self.logger.info("No project specified.")
@@ -185,11 +214,16 @@ class RedmineExporter:
 
         status_mapping = {
             1: "new",
-            2: "inprogress",
+            2: "in progress",
             3: "resolved",
             4: "feedback",
             5: "closed",
-            6: "rejected"
+            6: "rejected",
+            7: "approved",
+            8: "won't fix",
+            9: "re-opened",
+            10: "in view",
+            11: "ready for testing"
         }
 
         status_name = status_mapping.get(args.status, "any")
@@ -229,22 +263,32 @@ class RedmineExporter:
 
         rate_limiter = RateLimiter(self.rate_limit_delay)
 
-        for i, issue in enumerate(issues[current_issue_index:], current_issue_index + 1):
-            self.logger.info(f"Processing issue {i}/{len(issues)} (ID: {issue['id']})")
+        try:
+            for i, issue in enumerate(issues[current_issue_index:], current_issue_index + 1):
+                self.logger.info(f"Processing issue {i}/{len(issues)} (ID: {issue['id']})")
 
-            try:
-                comments = self.fetch_comments(issue["id"])
-                self.logger.info(f"Total comments found for issue {issue['id']}: {len(comments)}")
-                issue_data = self.prepare_issue_data(issue, comments)
-                self.export_data(issue_data, output_file)
-                rate_limiter.wait()
-                self.save_progress(i, progress_file)
-            except (RedmineAuthenticationError, RedminePermissionError, RedmineNotFoundError) as e:
-                self.logger.warning(str(e))
-            except Exception as e:
-                self.logger.error(f"Error processing issue {issue['id']}: {str(e)}")
+                try:
+                    comments = self.fetch_comments(issue["id"])
+                    self.logger.info(f"Total comments found for issue {issue['id']}: {len(comments)}")
+                    issue_data = self.prepare_issue_data(issue, comments)
+                    self.export_data(issue_data, output_file)
+                    rate_limiter.wait()
+                    self.save_progress(i, progress_file)
+                except (RedmineAuthenticationError, RedminePermissionError, RedmineNotFoundError) as e:
+                    self.logger.warning(str(e))
+                except Exception as e:
+                    self.logger.error(f"Error processing issue {issue['id']}: {str(e)}")
+
+        except KeyboardInterrupt:
+            self.logger.info("Script interrupted by user.")
+            sys.exit(0)
+        except (RedmineAuthenticationError, RedminePermissionError, RedmineNotFoundError) as e:
+            self.logger.warning(str(e))
+        except Exception as e:
+            self.logger.error(f"Error processing issue {issue['id']}: {str(e)}")
 
         self.logger.info("Issue export completed.")
+
 
 if __name__ == "__main__":
     exporter = RedmineExporter()
