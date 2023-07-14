b76545d4d5dc0406b34f648af15f947ef3aad4fa
Harrison Wilford <25427388+polterx@users.noreply.github.com>
2023-07-14 05:12:41 +0200

    Update _chunks.py
    Signed-off-by: Harrison Wilford <25427388+polterx@users.noreply.github.com>

--- utils/_chunks.py
+++ utils/_chunks.py
@@ -1,1 +1,5 @@
-
+# ############################################################################## #
+# Created by Polterx, on Saturday, 1st of July, 2023                             #
+# Website https://poltersanctuary.com                                            #
+# Github  https://github.com/PolterEnterprise                                    #
+# ############################################################################## #
