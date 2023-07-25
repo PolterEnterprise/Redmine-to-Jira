083159fabff56be1da85473d2a98fa77835b013a
Harrison Wilford <25427388+polterx@users.noreply.github.com>
2023-07-25 13:27:50 +0200

    _logging.py v1.0.3
    Signed-off-by: Harrison Wilford <25427388+polterx@users.noreply.github.com>

--- utils/_logging.py
+++ utils/_logging.py
@@ -4,34 +4,48 @@
 # Github  https://github.com/PolterEnterprise                                    #
 # ############################################################################## #
 
+import os
 import coloredlogs
 import logging
+
 from logging.handlers import RotatingFileHandler
 
-def configure_logging(debug_mode=False):
+DEFAULT_LOG_FORMAT = "[%(asctime)s] %(levelname)s %(message)s"
+DEFAULT_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+MAX_LOG_FILE_SIZE = 1000000  # 1MB
+BACKUP_COUNT = 3
+
+def configure_logging(debug_mode=False, log_format=DEFAULT_LOG_FORMAT, datefmt=DEFAULT_DATE_FORMAT):
     level = logging.DEBUG if debug_mode else logging.INFO
-    log_format = "[%(asctime)s] %(levelname)s %(message)s"
-    datefmt = "%Y-%m-%d %H:%M:%S"
 
     logging.basicConfig(level=level, format=log_format, datefmt=datefmt)
 
-    if not debug_mode:
-        coloredlogs.install(level=level, fmt=log_format, datefmt=datefmt)
-    else:
+    if debug_mode:
         coloredlogs.install(level=logging.INFO, fmt=log_format, datefmt=datefmt)
+    else:
+        coloredlogs.install(level=level, fmt=log_format, datefmt=datefmt)
 
-def setup_logger(log_file):
+def setup_logger(prefix, log_dir, log_file_name, log_format=DEFAULT_LOG_FORMAT, datefmt=DEFAULT_DATE_FORMAT):
+    # Create full log directory path with prefix
+    full_log_dir = os.path.join(log_dir, prefix)
+    
+    # Create log directory if it doesn't exist
+    os.makedirs(full_log_dir, exist_ok=True)
+
+    # Full path to the log file
+    log_file = os.path.join(full_log_dir, log_file_name)
+
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
 
-    log_format = "[%(asctime)s] %(levelname)s %(message)s"
-    log_date_format = "%Y-%m-%d %H:%M:%S"
-    formatter = logging.Formatter(log_format, log_date_format)
+    formatter = logging.Formatter(log_format, datefmt)
 
-    file_handler = RotatingFileHandler(log_file, maxBytes=1000000, backupCount=3)
+    file_handler = RotatingFileHandler(log_file, maxBytes=MAX_LOG_FILE_SIZE, backupCount=BACKUP_COUNT)
     file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(formatter)
 
     logger.addHandler(file_handler)
 
     return logger
+
+
